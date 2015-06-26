59eca1fe4975147be3ad0c771ba3cf58b123e6f4
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-06-26 19:48:48 +0800

    Update Main.php

--- src/MCPH/InfinitePopup/Main.php
+++ src/MCPH/InfinitePopup/Main.php
@@ -12,41 +12,35 @@ use MCPH\InfinitePopup\Tasks\PluginCallbackTask;
 class Main extends PluginBase
 {
    
-  public function onEnable(){
-        $this->getLogger()->info("InfinitePopup has been enabled.");
-        $cfg = $this->getConfig();
-        $this->getServer()->getScheduler()->scheduleRepeatingTask(new Tasks\PluginCallbackTask(array($this, "popup")),3); //Callbacktask still works lol
-
-  }
+   public function onEnable(){
+      $this->getLogger()->info("InfinitePopup has been enabled.");
+      $cfg = $this->getConfig();
+      $this->getServer()->getScheduler()->scheduleRepeatingTask(new Tasks\PluginCallbackTask(array($this, "popup")),3); //Callbacktask still works lol
+   }
   
-  public function onDisable(){
-        $this->getLogger()->info("InfinitePopup has been disabled.")
-
-  public function popup(){
+   public function onDisable(){
+      $this->getLogger()->info("InfinitePopup has been disabled.")
+   }
+   
+   public function popup(){
+      //$tn = substr($this->pmsg,(strlen($this->pmsg)-1)); //Other way
+      //$tc = substr($this->pmsg,0,(strlen($this->pmsg)-1));
        
-        //$tn = substr($this->pmsg,(strlen($this->pmsg)-1)); //Other way
-        //$tc = substr($this->pmsg,0,(strlen($this->pmsg)-1));
+      $this->lobby = $this->getServer()->getLevelByName($cfg->get("world-name"));
+      $this->pmsg = $cfg->get("popup-message");
+      $this->pmc = 0;
        
-        $this->lobby = $this->getServer()->getLevelByName($cfg->get("world-name"));
-        $this->pmsg = $cfg->get("popup-message");
-        $this->pmc = 0;
+      $tn = substr($this->pmsg, 1);
+      $tc = substr($this->pmsg, 1, 1);
        
-        $tn = substr($this->pmsg,1);
-        $tc = substr($this->pmsg,1,1);
+      $this->pmsg = $tn.$tc;
        
-        $this->pmsg = $tn.$tc;
-       
-        foreach($this->getServer()->getOnlinePlayers() as $ppp){
-           
-            if($ppp->getLevel() == $this->lobby){
-           
-                $ppp->sendPopup("\n§e   " . substr($this->pmsg,0,25));
-           
-            }
-        }
-       
-        $this->pmc++;
-       
+      foreach($this->getServer()->getOnlinePlayers() as $ppp){
+         if($ppp->getLevel() == $this->lobby){
+            $ppp->sendPopup(str_repeat(TextFormat::AQUA . "=" . TextFormat::YELLOW . "-", 25) . "\n§e   " . substr($this->pmsg, 0, 25));
+         }
+       }
+      $this->pmc++;
   }
 }
 
